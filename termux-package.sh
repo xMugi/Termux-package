a1e0c9ca9771e467ba3da4ef9a9ebf53f445b7d4
Mugi <54444757+xMugi@users.noreply.github.com>
2024-06-09 14:23:59 +0200

    Update termux-package.sh

--- termux-package.sh
+++ termux-package.sh
@@ -3,7 +3,7 @@
 export REPO_URL=https://github.com/xMugi/Termux-package.git
 export BRANCH=main
 
-pkg update && pkg upgrade -y && pkg install -y python php python-dev termux-services termux-tools tmux android-tools nodejs git perl bash nano ruby build-essential ffmpeg curl openssl openssh wget clang nmap w3m coreutils fish zip figlet cowsay unzip wcalc bmon unrar proot golang
+pkg update && pkg upgrade -y && pkg install -y python php termux-services termux-tools tmux android-tools nodejs git perl bash nano ruby build-essential ffmpeg curl openssl openssh wget clang nmap w3m coreutils fish zip figlet cowsay unzip wcalc bmon unrar proot golang
 
 
 git clone "${REPO_URL}"
