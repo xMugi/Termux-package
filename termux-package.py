58dfe5aa22ff4c406bc6a80f9f633068bd7d1db5
Mugi <54444757+xMugi@users.noreply.github.com>
2024-06-09 13:44:19 +0200

    Update termux-package.py

--- termux-package.py
+++ termux-package.py
@@ -11,14 +11,14 @@ This Command for access Storage in Termux
 [00] termux-setup-storage''')
 
 os.system("""
-apt update && apt upgrade -y &&
-apt install -y python python2 php python-dev python3 termux-service android-tools java git perl bash nano curl openssl openssh wget clang nmap w3m hydra ruby macchanger host dnsutils coreutils fish zip figlet cowsay unzip wcalc bmon unrar proot golang
+pkg update && pkg upgrade -y &&
+pkg install -y python php python-dev termux-services android-tools nodejs git perl bash nano curl openssl openssh wget clang nmap w3m rubydnsutils coreutils fish zip figlet cowsay unzip wcalc bmon unrar proot golang
 """)
 
-print ("Allow the Button For Access the Storage in Termux")
-
+print('''\033[96m
+Allow the Button For Access the Storage in Termux''')
 
 os.system ("termux-setup-storage")
 
 
-input("\n\nPress the enter key to exit : ")
+input("\033[96m\n\nPress the enter key to exit : ")
