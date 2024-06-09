2f19a587a7fc367819baa93c34f30518ecb7fc76
Mugi <54444757+xMugi@users.noreply.github.com>
2024-06-09 14:11:55 +0200

    Create termux-metasploit.sh

---
+++ termux-metasploit.sh
@@ -0,0 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+mkdir metasploit
+wget https://github.com/gushmazuko/metasploit_in_termux/raw/master/metasploit.sh
+chmod +x metasploit.sh
+./metasploit.sh
