5a51d4bc6873fc511ea5946c4c2a61a87feab131
Mugi <54444757+xMugi@users.noreply.github.com>
2024-06-09 14:17:09 +0200

    Update termux-metasploit.sh

--- termux-metasploit.sh
+++ termux-metasploit.sh
@@ -1,6 +1,25 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-mkdir metasploit
+# Check if we are already inside the 'metasploit' directory
+if [ "${PWD##*/}" = "metasploit" ]; then
+    echo "Already inside the 'metasploit' directory."
+else
+    # Check if the 'metasploit' directory exists
+    if [ ! -d "metasploit" ]; then
+        # Create the 'metasploit' directory
+        mkdir metasploit
+        echo "Directory 'metasploit' created."
+    fi
+    # Change to the 'metasploit' directory
+    cd metasploit
+    echo "Changed to the 'metasploit' directory."
+fi
+
+# Download the Metasploit script
 wget https://github.com/gushmazuko/metasploit_in_termux/raw/master/metasploit.sh
+
+# Make the script executable
 chmod +x metasploit.sh
+
+# Run the script
 ./metasploit.sh
