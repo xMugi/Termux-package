89f7df561ee8dbe63e15a3304d11e8b959303613
Mugi <54444757+xMugi@users.noreply.github.com>
2025-08-01 15:52:19 +0200

    added 2 new packages

--- termux-package.sh
+++ termux-package.sh
@@ -5,7 +5,7 @@ export BRANCH=main
 
 termux-setup-storage
 
-pkg update && pkg upgrade -y && pkg install -y python php termux-services termux-tools tmux android-tools nodejs git perl bash nano ruby build-essential ffmpeg curl openssl openssh wget clang nmap w3m coreutils fish zip figlet cowsay unzip wcalc bmon unrar proot golang
+pkg update && pkg upgrade -y && pkg install -y python php termux-services termux-tools tmux android-tools nodejs git perl bash nano ruby build-essential ffmpeg curl openssl openssh wget clang nmap w3m coreutils fish zip figlet cowsay unzip wcalc bmon unrar proot golang termux-wifi-connectioninfo dnsutils
 
 
 git clone "${REPO_URL}"
