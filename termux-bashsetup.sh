18c92950da514ec83625b42d30600aeaaaa4b059
Mugi <54444757+xMugi@users.noreply.github.com>
2026-03-31 18:54:59 +0200

    Update bash setup script to create symlink for .bashrc

--- termux-bashsetup.sh
+++ termux-bashsetup.sh
@@ -1,5 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/bash
-
+rm ~/.bashrc
 mv -v /data/data/com.termux/files/usr/etc/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc.backup
-
-cp bash.bashrc /data/data/com.termux/files/usr/etc && exit
+cp bash.bashrc /data/data/com.termux/files/usr/etc && ln -sf /data/data/com.termux/files/usr/etc/bash.rc ~/.bashrc && exit
