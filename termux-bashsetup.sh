e5d61bcbac86d8e422ca200f065510a49206dbd9
Mugi <54444757+xMugi@users.noreply.github.com>
2026-03-31 20:52:26 +0200

    Refactor bash setup script for clarity and structure

--- termux-bashsetup.sh
+++ termux-bashsetup.sh
@@ -1,4 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
 rm ~/.bashrc
 mv -v /data/data/com.termux/files/usr/etc/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc.backup
-cp bash.bashrc /data/data/com.termux/files/usr/etc && ln -sf /data/data/com.termux/files/usr/etc/bash.bashrc ~/.bashrc && exit
+cp bash.bashrc /data/data/com.termux/files/usr/etc
+ln -sf /data/data/com.termux/files/usr/etc/bash.bashrc ~/.bashrc
+exit
