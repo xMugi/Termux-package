2aac74e5f451a40f91386566c4346454e3f12e87
Mugi <54444757+xMugi@users.noreply.github.com>
2026-04-28 07:48:20 +0200

    Add neofetch configuration setup
    Create configuration directory for neofetch and copy config.

--- termux-bashsetup.sh
+++ termux-bashsetup.sh
@@ -3,4 +3,5 @@ rm ~/.bashrc
 mv -v /data/data/com.termux/files/usr/etc/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc.backup
 cp bash.bashrc /data/data/com.termux/files/usr/etc
 ln -sf /data/data/com.termux/files/usr/etc/bash.bashrc ~/.bashrc
+mkdir -p ~/.config/neofetch && cp neofetch.conf ~/.config/neofetch/config.conf
 exit
